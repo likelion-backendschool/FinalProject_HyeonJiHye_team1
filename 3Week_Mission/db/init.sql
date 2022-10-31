293ef4848c061f10f43aee76bf0c276be310b31d
zelymm <dkdlwkrz@naver.com>
2022-10-31 17:59:28 +0900

    chore: db 세팅 변경

--- 3Week_Mission/db/init.sql
+++ 3Week_Mission/db/init.sql
@@ -1,9 +1,9 @@
 # 테스트 DB 생성
-DROP DATABASE IF EXISTS final__2022_10_08__test;
-CREATE DATABASE final__2022_10_08__test;
-USE final__2022_10_08__test;
+DROP DATABASE IF EXISTS app3_ebook__test;
+CREATE DATABASE app3_ebook__test;
+USE app3_ebook__test;
 
 # 개발 DB 생성
-DROP DATABASE IF EXISTS final__2022_10_08__dev;
-CREATE DATABASE final__2022_10_08__dev;
-USE final__2022_10_08__dev;
+DROP DATABASE IF EXISTS app3_ebook__dev;
+CREATE DATABASE app3_ebook__dev;
+USE app3_ebook__dev;
