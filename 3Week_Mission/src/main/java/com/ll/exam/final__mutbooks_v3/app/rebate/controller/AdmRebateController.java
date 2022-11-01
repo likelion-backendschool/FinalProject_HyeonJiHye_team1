048f934a33da9a7a88566c77dcb0d8cb62cdb10c
zelymm <dkdlwkrz@naver.com>
2022-11-02 04:29:20 +0900

    feat: 정산 데이터 목록 구현

--- 3Week_Mission/src/main/java/com/ll/exam/final__mutbooks_v3/app/rebate/controller/AdmRebateController.java
+++ 3Week_Mission/src/main/java/com/ll/exam/final__mutbooks_v3/app/rebate/controller/AdmRebateController.java
@@ -1,14 +1,18 @@
 package com.ll.exam.final__mutbooks_v3.app.rebate.controller;
 
+import com.ll.exam.final__mutbooks_v3.app.rebate.entity.RebateOrderItem;
 import com.ll.exam.final__mutbooks_v3.app.rebate.service.RebateService;
+import com.ll.exam.final__mutbooks_v3.util.Ut;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,10 +29,22 @@ public class AdmRebateController {
 
     @PostMapping("/makeData")
     @PreAuthorize("hasAuthority('ADMIN')")
-    @ResponseBody
     public String makeData(String yearMonth) {
         rebateService.makeDate(yearMonth);
 
-        return "성공";
+        return "redirect:/adm/rebate/rebateOrderItemList?yearMonth=" + yearMonth + "&msg=" + Ut.url.encode("정산데이터가 성공적으로 생성되었습니다.");
+    }
+
+    @GetMapping("/rebateOrderItemList")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String showRebateOrderItemList(String yearMonth, Model model) {
+        if (yearMonth == null) {
+            yearMonth = "2022-11";
+        }
+        List<RebateOrderItem> items = rebateService.findRebateOrderItemsByPayDateIn(yearMonth);
+
+        model.addAttribute("items", items);
+
+        return "adm/rebate/rebateOrderItemList";
     }
 }
