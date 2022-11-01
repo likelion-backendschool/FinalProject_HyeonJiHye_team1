b40b8281faf5cf6dcb8971ce8ea258436c900745
zelymm <dkdlwkrz@naver.com>
2022-11-02 02:20:49 +0900

    feat: 정산 데이터 생성 & 처리

--- 3Week_Mission/src/main/java/com/ll/exam/final__mutbooks_v3/app/rebate/controller/AdmRebateController.java
+++ 3Week_Mission/src/main/java/com/ll/exam/final__mutbooks_v3/app/rebate/controller/AdmRebateController.java
@@ -1,16 +1,34 @@
 package com.ll.exam.final__mutbooks_v3.app.rebate.controller;
 
+import com.ll.exam.final__mutbooks_v3.app.rebate.service.RebateService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/adm/rebate")
+@Slf4j
 public class AdmRebateController {
+    private final RebateService rebateService;
+
     @GetMapping("/makeData")
     @PreAuthorize("hasAuthority('ADMIN')")
     public String showMakeData() {
         return "adm/rebate/makeData";
+    }
+
+    @PostMapping("/makeData")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseBody
+    public String makeData(String yearMonth) {
+        rebateService.makeDate(yearMonth);
+
+        return "성공";
     }
 }
