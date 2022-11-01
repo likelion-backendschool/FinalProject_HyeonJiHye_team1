7b2ca442ba5b27d95e1a746cdde9951134391516
zelymm <dkdlwkrz@naver.com>
2022-11-02 05:45:25 +0900

    feat: 정산 시스템 구현 - 전체, 건별, 선택 옵션

--- 3Week_Mission/src/main/java/com/ll/exam/final__mutbooks_v3/app/rebate/entity/RebateOrderItem.java
+++ 3Week_Mission/src/main/java/com/ll/exam/final__mutbooks_v3/app/rebate/entity/RebateOrderItem.java
@@ -103,16 +103,21 @@ public class RebateOrderItem extends BaseEntity {
     }
 
     public int calculateRebatePrice() {
-        if ( isRebateAvailable() == false ) {
+        if (refundPrice > 0) {
             return 0;
         }
         return wholesalePrice - pgFee;
     }
 
     public boolean isRebateAvailable() {
-        if ( refundPrice > 0 ) {
+        if (refundPrice > 0 || rebateDate != null) {
             return false;
         }
         return true;
+    }
+
+    public void setRebateDone(long cashLogId) {
+        rebateDate = LocalDateTime.now();
+        this.rebateCashLog = new CashLog(cashLogId);
     }
 }
